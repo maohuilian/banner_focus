a27049e6d47f80febab9ecd9a591b04e44ed911a
zpy <657210800@qq.com>
2017-05-09 17:56:23 +0800

    插件修复

--- static/js/jquery.focus.js
+++ static/js/jquery.focus.js
@@ -12,15 +12,14 @@
         // 追加箭头
         $obj.append('<div class="direction"><div class="arrows_left"></div><div class="arrows_right"></div></div>');
         // 追加原点
-        $obj.append('<ol class="num"><li class="on">1</li><li>2</li><li>3</li><li>4</li></ol>');
-        var $arrow_right = $obj.find(".arrows_right"),
+        $obj.append('<ol class="num"></ol>');
+        var $num_on = $obj.children('ol.num'),
+            $arrow_right = $obj.find(".arrows_right"),
             $arrow_left = $obj.find(".arrows_left"),
-            $num_on = $(".num"),
-            $num_li = $(".num li"),
             $pic_li = $(".picture li"),
             t, // 定时器
             $pic_li_length = $(".picture li").size();
-
+            
         //遍历图片上的文字
         $pic_li.each(function() {
             var index = $(this).index();
@@ -32,19 +31,25 @@
             $(this).append('<p>'+ alt +'</p><strong>'+ title +'</strong>');
             $obj.css({width:m.c_width,height:parseInt(140)+ parseInt(m.c_height)});
         });
-       
-        //点击数字触发
-        tigger_num();
-        function tigger_num() {
-            $num_li.each(function() {
-                var index = $(this).index();
-                $(this).on(m.event,function() {
-                    stop();
-                    numOn(index);
-                    speed(index);
-                });
-            });
+      
+        for(i =1;i<=$pic_li_length;i++){
+            $num_on.append('<li>'+i+'</li>');
         };
+        var rr = $obj.children('ol.num');
+        var rrLi = rr.children('li');
+        //点击数字触发
+        rrLi.each(function() {
+            var index = $(this).index();
+            if(index == 0){
+                $(this).addClass('on').siblings('li').removeClass('on');
+            }
+            $(this).on(m.event,function() {
+                stop();
+                $(this).addClass('on').siblings('li').removeClass('on');
+                speed(index);
+            });
+        });
+       
         //点击向右箭头
         $arrow_right.click(function () {
             changePic('R');
@@ -55,7 +60,7 @@
         });
         //点击箭头触发事件
         function changePic(arrow) {
-            var n = $num_on.children('li.'+ m.current).index(); //3 0 1 2     -1 0 1 2
+            var n = rr.children('li.on').index(); //3 0 1 2     -1 0 1 2
             var i;
             if(arrow == 'R') {
                 i = n + 1; // 1 2 3 4
@@ -76,10 +81,10 @@
         };
         //相对应的数字按钮
         function numOn (n) {
-            $num_li.eq(n).addClass(m.current).siblings('li').removeClass(m.current);
+            rrLi.eq(n).addClass(m.current).siblings('li').removeClass(m.current);
         };
         //图片轮播
-        roll()
+        roll();
         function roll () {
             t = setInterval(function() {changePic('R')},m.time);
         }
